f27510808ab8faeb12147fe40ad1a662f9bd55db
realust <35360039+realust@users.noreply.github.com>
2018-01-16 22:20:24 +0900

    작업사항
    1. IntroActivity : 2초간 로고를 띄우고, 2초 경과 후 메인 액티비티로 이동
    2. MainActivity : 미구현
    3. logo.png : 현재 로고/화면에 꽉 차게 제작
    4. AndroidManifest.xml : 현재 introactivity, mainactivity만 기입. introactivity가 스타팅 액티비티

---
+++ IntroActivity.java
@@ -0,0 +1,26 @@
+package com.threeinone.toktok;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+public class IntroActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, 2000);
+    }
+}
