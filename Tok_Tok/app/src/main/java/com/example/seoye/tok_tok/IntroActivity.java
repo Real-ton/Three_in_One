2dc3437d37ec2b268b9e729a84486271340be971
seo <dltjdp9792@naver.com>
2018-01-17 17:51:00 +0900

    change2

--- Tok_Tok/app/src/main/java/com/example/seoye/tok_tok/IntroActivity.java
+++ Tok_Tok/app/src/main/java/com/example/seoye/tok_tok/IntroActivity.java
@@ -4,11 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 public class IntroActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        출처: http://commin.tistory.com/63 [Commin의 일상코딩]
         setContentView(R.layout.activity_intro);
 
         Handler handler = new Handler();
--- Tok_Tok/app/src/main/java/com/example/seoye/tok_tok/MainActivity.kt
+++ Tok_Tok/app/src/main/java/com/example/seoye/tok_tok/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.seoye.tok_tok
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
     }
 }
