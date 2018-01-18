6d7b2332313cab8a299bbea9e3e58524e509642b
seo <dltjdp9792@naver.com>
2018-01-19 05:18:43 +0900

    activity change

---
+++ Tok_Tok/app/src/main/java/com/example/seoye/tok_tok/ChoiceActivity.kt
@@ -0,0 +1,15 @@
+package com.example.seoye.tok_tok
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+
+class ChoiceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_choice)
+    }
+}
--- Tok_Tok/app/src/main/java/com/example/seoye/tok_tok/MainActivity.kt
+++ Tok_Tok/app/src/main/java/com/example/seoye/tok_tok/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.seoye.tok_tok
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.WindowManager
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,9 @@ class MainActivity : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        st_bt.setOnClickListener{
+            val nextIntent = Intent(this, ChoiceActivity::class.java)
+            startActivity(nextIntent)
+        }
     }
 }
